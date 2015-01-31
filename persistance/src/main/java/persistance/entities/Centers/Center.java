a8f44fa3867b65604759bd2a76c54246756e5cae
dramolbhalla <amol.bhalla@asu.edu>
2015-01-30 19:01:26 -0700

    Centers Entities
    Updating the centers tables

--- persistance/src/main/java/persistance/entities/Centers/Center.java
+++ persistance/src/main/java/persistance/entities/Centers/Center.java
@@ -1,5 +1,93 @@
 package persistance.entities.Centers;
 
 public class Center {
+	
+	int cid;
+	String centerId;
+	String centerNumber;  
+	String centerName; 
+	String centerContactTitle; 
+	String centerContactFirstName; 
+	String centerContactLastName; 
+	String centerContactJobTitle; 
+	String centerAddress1; 
+	String centerAddress2; 
+	String centerCity; 
+	String centerState;  
+	String centerZip; 
+	String centerPhone; 
+	String centerFax; 
+	String centerNote;
+	
+	public int get_cid (){
+		return cid;
+	}
+	
+	public void set_cid( int cid){
+		this.cid = cid;
+	}
+	
+	public String get_centerId(){
+		return centerId;
+	}
+	
+	public void set_centerId(String centerId){
+		this.centerId = centerId;
+	}
+	
+	public String get_centerNumber(){
+		return centerNumber;
+	}
+	
+	public void set_centerNumber(String centerNumber){
+		this.centerNumber = centerNumber;
+	}
+	
+	public String get_centerName(){
+		return centerName;
+	}
+	
+	public void set_centerName(String centerName){
+		this.centerName = centerName;
+	}
 
+	public String get_centerContactTitle(){
+		return centerContactTitle;
+	}
+	
+	public void set_centerContactTitle(String centerContactTitle){
+		this.centerContactTitle = centerContactTitle;
+	}
+	
+	public String get_centerContactFirstName(){
+		return centerContactFirstName;
+	}
+	
+	public void set_centerContactFirstName(String centerContactFirstName){
+		this.centerContactFirstName = centerContactFirstName;
+	}
+	
+	public String get_centerContactLastName(){
+		return centerContactLastName;
+	}
+	
+	public void set_centerContactLastName(String centerContactLastName){
+		this.centerContactLastName = centerContactLastName;
+	}
+	
+	public String get_centerContactJobTitle(){
+		return centerContactJobTitle;
+	}
+	
+	public void set_centerContactJobTitle(String centerContactJobTitle){
+		this.centerContactJobTitle = centerContactJobTitle;
+	}
+	
+	public String get_centerAddress1(){
+		return centerAddress1;
+	}
+	
+	public void set_centerAddress1(String centerAddress1){
+		this.centerAddress1 = centerAddress1;
+	}
 }
--- persistance/src/main/java/persistance/entities/Clinics/Clinic.java
+++ persistance/src/main/java/persistance/entities/Clinics/Clinic.java
@@ -2,4 +2,20 @@ package persistance.entities.Clinics;
 
 public class Clinic {
 
+	int cid;
+	String clinicId;
+	String clinicNumber;  
+	String clinicName; 
+	String clinicContactTitle; 
+	String clinicContactFirstName; 
+	String clinicContactLastName; 
+	String clinicContactJobTitle; 
+	String clinicAddress1; 
+	String clinicAddress2; 
+	String clinicCity; 
+	String clinicState;  
+	String clinicZip; 
+	String clinicPhone; 
+	String clinicFax; 
+	String clinicNote;
 }
