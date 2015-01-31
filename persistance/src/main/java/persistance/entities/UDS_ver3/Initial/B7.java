7bb7c5ff473fa02d8b34436cd1ce0f93260606ed
dramolbhalla <amol.bhalla@asu.edu>
2015-01-31 12:26:36 -0700

    UDS Initial Packet Entities
    Done with all the variable definitions, now now going to start the
    getter and setters.

--- persistance/src/main/java/persistance/entities/UDS_ver3/Initial/B7.java
+++ persistance/src/main/java/persistance/entities/UDS_ver3/Initial/B7.java
@@ -5,5 +5,14 @@ public class B7 {
 	int sid;
 	String userId;
 	String subjectId;
-
+	int bills;
+	int taxes;
+	int shopping;
+	int games;
+	int stove;
+	int mealprep;
+	int events;
+	int payattn;
+	int remdates;
+	int travel;
 }
--- persistance/src/main/java/persistance/entities/UDS_ver3/Initial/D2.java
+++ persistance/src/main/java/persistance/entities/UDS_ver3/Initial/D2.java
@@ -5,5 +5,37 @@ public class D2 {
 	int sid;
 	String userId;
 	String subjectId;
-
+	int cancer;
+	int cancsite;
+	int diabet;
+	int myoinf;
+	int conghrt;
+	int afibrill;
+	int hypert;
+	int angina;
+	int hypchol;
+	int vb12def;
+	int thydis;
+	int arth;
+	int artype;
+	String artypex;
+	int artupex;
+	int artloex;
+	int artspin;
+	int artunkn;
+	int urineinc;
+	int bowlic;
+	int sleepap;
+	int remdis;
+	int hyposom;
+	int sleepoth;
+	int sleeptx;
+	int angiocp;
+	int angiopci;
+	int pacemake;
+	int hvalve;
+	int antienc;
+	String antiencx;
+	int othcond;
+	String othcondx;
 }
